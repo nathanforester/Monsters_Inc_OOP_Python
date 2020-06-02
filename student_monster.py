0d4a730195b7e838938322f8baa3e5ae2beb6531
Nathan Forester <49757908+nathanforester@users.noreply.github.com>
2020-06-02 08:37:38 +0100

    Update student_monster.py

--- student_monster.py
+++ student_monster.py
@@ -11,14 +11,18 @@ class StudentMonster(Monster):
 
     def get_student_number(self):
         return self.__student_number
+    
+    def set_student_number(self, new_student_number):
+        self.__student_number = new_student_number
+        return new_student_number
 
     def get_skill(self):
         return self.__skill_list
 
-    def add_skill(self, new_skill):
+    def add_skill(self, new_skill, new_student_number):
         self.__skill_list = new_skill
         new_skill = input('enter a scary skill, muahahahaha!: ')
-        return new_skill
+        return new_skill + new_student_number
 
 
 
